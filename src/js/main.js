6de4ef922a24f253ba0087cdfe5f6f307a5af212
Jorge Filho <jorge.munizfilho@gmail.com>
2025-10-17 15:58:37 -0300

    created the task delete function

--- src/js/main.js
+++ src/js/main.js
@@ -14,16 +14,37 @@ inputTask.addEventListener("keypress", (e) => {
   }
 });
 
+const cleanInput = () => {
+  inputTask.value = "";
+  inputTask.focus();
+};
+
+const createDeleteButton = (itemTask) => {
+  itemTask.innerText += " ";
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Apagar";
+  //   deleteButton.classList.add("delete")
+  deleteButton.setAttribute("class", "delete");
+  itemTask.appendChild(deleteButton);
+};
+
 const createTask = (textInpu) => {
   const itemTask = createItemTask();
-
   itemTask.innerText = textInpu;
-
   tasks.appendChild(itemTask);
+  cleanInput();
+  createDeleteButton(itemTask);
 };
 
 btnTask.addEventListener("click", () => {
   if (!inputTask.value) return;
 
   createTask(inputTask.value);
+});
+
+document.addEventListener("click", (e) => {
+  const element = e.target;
+  if (element.classList.contains("delete")) {
+    element.parentElement.remove();
+  }
 });
